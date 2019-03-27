8ba09acc8721d81824574493473716db96ffc28b
Jeff Reiner <theundeadghost@gmail.com>
2019-03-27 17:01:39 +0100

    :sparkles: update with better height n such

--- src/components/Theme/theme.js
+++ src/components/Theme/theme.js
@@ -1,7 +1,15 @@
 export default {
   breakpoints: ["40em", "52em", "64em"],
   fontSizes: [12, 14, 16, 20, 24, 32, 48, 64],
-  colors: {},
+  colors: {
+    primary: "#00BBF0",
+    secondary: "#015792",
+    tertiary: "#F2F4FB",
+    experts: "#6E45BC",
+    productDesign: "#00C5FF",
+    ux: "#0066C4",
+    text: "#2D3B4A"
+  },
   fontWeights: {
     normal: 400,
     black: 900
--- src/pages/index.js
+++ src/pages/index.js
@@ -15,8 +15,21 @@ const Container = props => (
   />
 );
 
+const Grid = props => (
+  <Box
+    {...props}
+    css={css`
+      display: grid;
+      grid-template-rows: 100vh;
+    grid-auto-rows: 50vh;
+      align-items: center;
+    `}
+  />
+);
+
 const ColoredPanel = () => (
   <Box
+    bg="tertiary"
     css={css`
       position: absolute;
       left: 0;
@@ -24,11 +37,11 @@ const ColoredPanel = () => (
       width: 100vw;
       height: 200vh;
       z-index: -1;
-      background-image: radial-gradient(
+      /* background-image: radial-gradient(
         farthest-side circle at bottom left,
         #f87998,
         #fdc67e
-      );
+      ); */
       border-radius: 0.5em;
 
       display: none;
@@ -42,7 +55,7 @@ const ColoredPanel = () => (
 const Link = props => (
   <RebassLink
     fontSize={3}
-    color="black"
+    color="text"
     css={css`
       font-family: Sneak, sans-serif;
       text-decoration: none;
@@ -61,17 +74,17 @@ const Wordmark = ({ primary, secondary }) => (
     letterSpacing={0.44}
     fontSize={32}
     fontFamily="libreFranklin"
-    color={["black", null, primary]}
+    color={primary}
   >
     DeepWork.
-    <Text as="span" color={["black", null, secondary]}>
+    <Text as="span" color={secondary}>
       Studio
     </Text>
   </Text>
 );
 
 const Paragraph = props => (
-  <Text as="p" fontSize={3} fontFamily="sneak" {...props} />
+  <Text as="p" fontSize={3} color="text" fontFamily="sneak" {...props} />
 );
 
 const IndexPage = () => (
@@ -79,67 +92,78 @@ const IndexPage = () => (
     <SEO />
 
     <Container>
-      <Box as="section" my={["25vh", null, "35vh"]} px={3}>
-        <Wordmark primary="#FF9487" secondary="#FF7395" />
-        <Heading
-          as="h1"
-          fontFamily="sneak"
-          fontWeight="black"
-          fontSize={[5, 6]}
-          lineHeight={1.5}
-          css={{ maxWidth: 720 }}
-          mt={4}
-        >
-          Releasing User-Focused Blockchain Products
-        </Heading>
-      </Box>
-
-      <Flex
-        as="section"
-        justifyContent="flex-end"
-        my={["25vh", null, "35vh"]}
-        px={3}
-      >
-        <Box
-          width={["100%", null, "65%"]}
-          pl={[0, null, 5]}
-          pb={[0, null, 5]}
-          css={{ position: "relative" }}
-        >
-          <ColoredPanel />
-          <Paragraph mb={0} lineHeight={1.5}>
-            We are a team of cross-functional experts condensing years of
-            product design and UX experience into delightful deliverables to
-            teams like you.
-          </Paragraph>
-        </Box>
-      </Flex>
-
-      <Box as="section" my={["25vh", null, "35vh"]} px={3}>
-        <Heading
-          as="h2"
-          fontFamily="sneak"
-          fontWeight="black"
-          fontSize={[5, 6]}
-          lineHeight={1.2}
-          mb={4}
-        >
-          Say Hello
-        </Heading>
-        <Flex flexDirection={["column", "row"]}>
-          <Link mb={[3, 0]} mr={[0, 4]} href="mailto:hey@deepwork.studio">
-            hey@deepwork.studio
-          </Link>
-          <Link
-            mb={[3, 0]}
-            mr={[0, 4]}
-            href="https://twitter.com/deepwork_studio"
+      <Grid>
+        <Box as="section" px={3}>
+          <Wordmark primary="primary" secondary="secondary" />
+          <Heading
+            as="h1"
+            fontFamily="sneak"
+            fontWeight="black"
+            color="text"
+            fontSize={[5, 6]}
+            lineHeight={1.5}
+            css={{ maxWidth: 720 }}
+            mt={4}
           >
-            @deepwork_studio
-          </Link>
-          {/* <Link href="">Meet the team on Notion</Link> */}
-        </Flex>
-      </Box>
+            Releasing User-Focused Blockchain Products
+          </Heading>
+        </Box>
+
+        <Box as="section" px={3}>
+          <Flex justifyContent="flex-end">
+            <Box
+              width={["100%", null, "65%"]}
+              pl={[0, null, 5]}
+              pb={[0, null, 5]}
+              css={{ position: "relative" }}
+            >
+              <ColoredPanel />
+              <Paragraph mb={0} lineHeight={1.5}>
+                We are a team of cross-functional{" "}
+                <Text as="span" fontWeight="black" color="experts">
+                  experts
+                </Text>{" "}
+                condensing years of{" "}
+                <Text as="span" fontWeight="black" color="productDesign">
+                  product design
+                </Text>{" "}
+                and{" "}
+                <Text as="span" fontWeight="black" color="ux">
+                  UX
+                </Text>{" "}
+                experience into delightful deliverables to teams like you.
+              </Paragraph>
+            </Box>
+          </Flex>
+        </Box>
+
+        <Box as="section" px={3}>
+          <Heading
+            as="h2"
+            fontFamily="sneak"
+            fontWeight="black"
+            color="text"
+            fontSize={[5, 6]}
+            lineHeight={1.2}
+            mb={4}
+          >
+            Say Hello
+          </Heading>
+          <Flex flexDirection={["column", "row"]}>
+            <Link mb={[3, 0]} mr={[0, 4]} href="mailto:hey@deepwork.studio">
+              hey@deepwork.studio
+            </Link>
+            <Link
+              mb={[3, 0]}
+              mr={[0, 4]}
+              href="https://twitter.com/deepwork_studio"
+            >
+              @deepwork_studio
+            </Link>
+            {/* <Link href="">Meet the team on Notion</Link> */}
+          </Flex>
+        </Box>
+      </Grid>
     </Container>
   </Layout>
 );
