f36c7ec33f1b0b7e10a635f3b2bd110a9441d470
Jeff Reiner <theundeadghost@gmail.com>
2019-03-29 17:06:21 +0100

    :sparkles: adding nice font smoothing

--- src/components/Theme/index.js
+++ src/components/Theme/index.js
@@ -1,10 +1,22 @@
 import React from "react";
-import { ThemeProvider } from "styled-components";
+import { ThemeProvider, createGlobalStyle } from "styled-components";
 
 import theme from "./theme";
 
+const GlobalStyle = createGlobalStyle`
+  body {
+    -webkit-font-smoothing: antialiased;
+	  -moz-osx-font-smoothing: grayscale;
+  }
+`;
+
 const Theme = props => (
-  <ThemeProvider theme={theme}>{props.children}</ThemeProvider>
+  <ThemeProvider theme={theme}>
+    <>
+      <GlobalStyle theme={theme} />
+      {props.children}
+    </>
+  </ThemeProvider>
 );
 
 export default Theme;
