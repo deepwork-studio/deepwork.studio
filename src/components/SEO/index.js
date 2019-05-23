c243c2e4cc85d295e8a000db7e0447321b43dd3e
Jeff Reiner <theundeadghost@gmail.com>
2019-05-23 22:14:39 +0200

    :art: running prettier

--- src/components/SEO/index.js
+++ src/components/SEO/index.js
@@ -74,8 +74,11 @@ const SEO = ({ description, lang, meta, title }) => {
       <script type="application/ld+json">
         {JSON.stringify(schemaOrgWebPage)}
       </script>
-      <script async defer src="https://cdn.noticeable.io/v1/noticeable-widget.js"></script>
-
+      <script
+        async
+        defer
+        src="https://cdn.noticeable.io/v1/noticeable-widget.js"
+      />
     </Helmet>
   );
 };
